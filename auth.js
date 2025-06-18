8ea24297263d7b0b34ad75517052586249163c04
James Elazegui <jameselazegui21@gmail.com>
2025-06-18 16:30:30 +1200

    Created a function to handle generation of session tokens

--- auth.js
+++ auth.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const ms = require('ms');
 const db = require('./db');
-const { generateAccessToken, generateRefreshToken } = require('./functions');
+const { generateSessionTokens } = require('./functions');
 
 const router = express.Router();
 
@@ -28,43 +27,14 @@ router.post('/login', (req, res) => {
     if (password === row.password) {
       // issue access token and refresh token
       const id = row.id;
-      const accessToken = generateAccessToken(row);
-      const refreshToken = generateRefreshToken(row);
 
-      const sqlRefreshTokenToDB = `
-      INSERT INTO refresh_tokens (user_id, token) 
-      VALUES (?, ?) 
-      ON CONFLICT(user_id) 
-      DO UPDATE SET token = excluded.token, created_at = CURRENT_TIMESTAMP;
-      `;
-
-      // save refresh token with id to database
-      db.run(sqlRefreshTokenToDB, [id, refreshToken], (err) => {
-        if (err) {
-          return res.status(500).json({
-            error: 'Failed to save refresh token to database',
-            message: err.message,
-          });
-        }
-      });
-
-      // save refresh token to cookie
-      res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'Strict',
-        maxAge: ms(process.env.REFRESH_TOKEN_EXPIRES_IN),
-      });
-
-      return res.status(200).json({
-        message: 'Login success',
-        user: { username: row.username, id: id },
-        accessToken: accessToken, // attach access token to res - this goes to client memory
-      });
+      generateSessionTokens(id, row, res);
     } else {
       res.status(401).json({ message: 'Incorrect password' });
     }
   });
 });
+
+router.post('/token', (req, res) => {});
 
 module.exports = router;
--- functions.js
+++ functions.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const ms = require('ms');
+const db = require('./db');
 
 // Compare function object A and object B keys
 function checkKeys(a, b) {
@@ -28,4 +30,49 @@ function generateRefreshToken(user) {
   });
 }
 
-module.exports = { checkKeys, generateAccessToken, generateRefreshToken };
+/*
+generate session tokens (save new refresh token to db, save new refresh token to client cookie, issue new access token and send to res.json )
+*/
+function generateSessionTokens(id, row, res) {
+  const accessToken = generateAccessToken(row);
+  const refreshToken = generateRefreshToken(row);
+
+  const sqlRefreshTokenToDB = `
+      INSERT INTO refresh_tokens (user_id, token) 
+      VALUES (?, ?) 
+      ON CONFLICT(user_id) 
+      DO UPDATE SET token = excluded.token, created_at = CURRENT_TIMESTAMP;
+      `;
+
+  // save refresh token with id to database
+  db.run(sqlRefreshTokenToDB, [id, refreshToken], (err) => {
+    if (err) {
+      return res.status(500).json({
+        error: 'Failed to save refresh token to database',
+        message: err.message,
+      });
+    }
+  });
+
+  // save refresh token to cookie
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'Strict',
+    maxAge: ms(process.env.REFRESH_TOKEN_EXPIRES_IN),
+  });
+
+  // return a new access token to res.json
+  return res.status(200).json({
+    message: 'Login success',
+    user: { username: row.username, id: id },
+    accessToken: accessToken, // attach access token to res - this goes to client memory
+  });
+}
+
+module.exports = {
+  checkKeys,
+  generateAccessToken,
+  generateRefreshToken,
+  generateSessionTokens,
+};
