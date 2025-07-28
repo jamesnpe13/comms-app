1c95e19ed8921f0ada7b7d1dd5f5c7c108769af3
James Elazegui <jameselazegui21@gmail.com>
2025-07-28 14:01:23 +1200

    Strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -9,15 +9,15 @@ import { MessagingProvider } from './context/MessagingContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <MessagingProvider>
-    <BrowserRouter>
-      <NotifProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </NotifProvider>
-    </BrowserRouter>
-  </MessagingProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <MessagingProvider>
+      <BrowserRouter>
+        <NotifProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </NotifProvider>
+      </BrowserRouter>
+    </MessagingProvider>
+  </React.StrictMode>
 );
