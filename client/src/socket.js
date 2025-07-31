dc495c8e252dc13ceebb55839ee1de54ddd0a46b
James Elazegui <jameselazegui21@gmail.com>
2025-07-31 22:57:38 +1200

    api_base_url in client update.

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const apiBaseUrl = 'http://localhost:5000';
-// const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+// const apiBaseUrl = 'http://localhost:5000';
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 const socket = io(apiBaseUrl, {
   transports: ['websocket', 'polling'],
