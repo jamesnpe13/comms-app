dd6aeb147a89796d350e32444061628141c60510
James Elazegui <jameselazegui21@gmail.com>
2025-08-03 16:51:16 +1200

    update

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const apiBaseUrl = process.env.REACT_APP_API_URL;
+// const apiBaseUrl = process.env.REACT_APP_API_URL;
 const socketIoUrl = process.env.REACT_APP_SOCKET_IO_URL;
 
 const socket = io(socketIoUrl, {
